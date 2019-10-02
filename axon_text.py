f664884967cd88161fd12fa54a3c36bb052e347d
Christian Rickert <mail@crickert.de>
2019-10-01 21:15:14 -0600

    Create axon_text.py

---
+++ axon_text.py
@@ -0,0 +1,39 @@
+"""
+Axon Text File (ATF) format module
+Copyright (C) 2019 Christian Rickert <mail@crickert.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNBESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA.
+
+Version 1.0
+"""
+
+# imports
+import numpy as np
+
+
+# functions
+
+def read():
+    """ read the information of an ATF file into a list """
+    pass
+
+def write():
+    """ write the information of a list into an ATF file """
+    pass
+
+def merge():
+    """ merge two ATF files with the option of continous data records """
+    pass
